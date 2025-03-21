aec7fa2710cd936acf3b5ab29377c97d03e80c53
Anqi Qiu <47854643+Zhao-Pingyu@users.noreply.github.com>
2025-03-20 23:44:58 -0400

    Update atomsk.sh

--- atomsk.sh
+++ atomsk.sh
@@ -4,9 +4,9 @@
 'atomsk.exe' NiAl3-ortho.xsf -swap X Z NiAl3_swap.xsf
 for ((i=0;i<=180;i+=1))
 do 
-	'atomsk.exe' --polycrystal Al_swap.xsf Al-$i.txt Al-$i.lmp;
+    'atomsk.exe' --polycrystal Al_swap.xsf Al-$i.txt Al-$i.lmp;
     'atomsk.exe' Al-$i.lmp -select in box 50 50 50 150 150 150 -rmatom select Al-outbox-$i.lmp;
     'atomsk.exe' --polycrystal NiAl3_swap.xsf Al3Ni-$i.txt Al3Ni-$i.lmp;
     'atomsk.exe' Al3Ni-$i.lmp -select out box 50 50 50 150 150 150 -rmatom select NiAl3-inbox-$i.lmp;
-	'atomsk.exe' --merge 2 Al-outbox-$i.lmp Al3Ni-inbox-$i.lmp Al-Al3Ni-$i.lmp;
+    'atomsk.exe' --merge 2 Al-outbox-$i.lmp Al3Ni-inbox-$i.lmp Al-Al3Ni-$i.lmp;
 done
